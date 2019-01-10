c00461c5d08bfd1f4e59e381b2345917da09c2b7
yezhengyuan <yezhengyuan@cmcm.com>
2019-01-10 17:52:12 +0800

    add zwzh.python

---
+++ tensor2tensor/test_data/yzy_usr_dir/translate_ende_news14.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+# Copyright 2018 The Tensor2Tensor Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Data generators for translation data-sets."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import tarfile
+
+# Dependency imports
+
+from tensor2tensor.data_generators import generator_utils
+from tensor2tensor.data_generators import problem
+from tensor2tensor.data_generators import text_encoder
+from tensor2tensor.data_generators import text_problems
+from tensor2tensor.data_generators import translate
+from tensor2tensor.utils import registry
+
+import tensorflow as tf
+
+_ENDE_TRAIN_DATASETS = [
+    [
+        "http://data.statmt.org/wmt17/translation-task/training-parallel-nc-v12.tgz",  # pylint: disable=line-too-long
+        ("training/news-commentary-v12.de-en.en",
+         "training/news-commentary-v12.de-en.de")
+    ],
+    [
+        "http://www.statmt.org/wmt13/training-parallel-commoncrawl.tgz",
+        ("commoncrawl.de-en.en", "commoncrawl.de-en.de")
+    ],
+    [
+        "http://www.statmt.org/wmt13/training-parallel-europarl-v7.tgz",
+        ("training/europarl-v7.de-en.en", "training/europarl-v7.de-en.de")
+    ],
+]
+_ENDE_TEST_DATASETS = [
+    [
+        "http://data.statmt.org/wmt17/translation-task/dev.tgz",
+        ("dev/newstest2014-deen-src.en.sgm", "dev/newstest2014-deen-ref.de.sgm")
+    ],
+]
+
+
+@registry.register_problem
+class TranslateEndeWmt8kNews14(translate.TranslateProblem):
+  """Problem spec for WMT En-De translation."""
+
+  @property
+  def approx_vocab_size(self):
+    return 2**13  # 8192
+
+  @property
+  def vocab_filename(self):
+    return "vocab.ende.%d" % self.approx_vocab_size
+
+  def source_data_files(self, dataset_split):
+    train = dataset_split == problem.DatasetSplit.TRAIN
+    return _ENDE_TRAIN_DATASETS if train else _ENDE_TEST_DATASETS
+
+
+@registry.register_problem
+class TranslateEndeWmt32kNews14(TranslateEndeWmt8kNews14):
+
+  @property
+  def approx_vocab_size(self):
+    return 2**15  # 32768
+
